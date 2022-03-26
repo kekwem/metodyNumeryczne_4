a2561d600af892670f5062fa62927b1204a95d03
kekwem <93526125+kekwem@users.noreply.github.com>
2022-03-26 15:44:05 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,431 @@
+﻿
+namespace interpolacja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x_0_x = new System.Windows.Forms.TextBox();
+            this.x_0_y = new System.Windows.Forms.TextBox();
+            this.x_1_y = new System.Windows.Forms.TextBox();
+            this.x_1_x = new System.Windows.Forms.TextBox();
+            this.x_2_y = new System.Windows.Forms.TextBox();
+            this.x_2_x = new System.Windows.Forms.TextBox();
+            this.x_3_y = new System.Windows.Forms.TextBox();
+            this.x_3_x = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.v = new System.Windows.Forms.Label();
+            this.liczba = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.first = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.third = new System.Windows.Forms.Label();
+            this.fought = new System.Windows.Forms.Label();
+            this.x_i = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // x_0_x
+            // 
+            this.x_0_x.Location = new System.Drawing.Point(71, 40);
+            this.x_0_x.Name = "x_0_x";
+            this.x_0_x.Size = new System.Drawing.Size(100, 22);
+            this.x_0_x.TabIndex = 0;
+            // 
+            // x_0_y
+            // 
+            this.x_0_y.Location = new System.Drawing.Point(177, 40);
+            this.x_0_y.Name = "x_0_y";
+            this.x_0_y.Size = new System.Drawing.Size(100, 22);
+            this.x_0_y.TabIndex = 1;
+            // 
+            // x_1_y
+            // 
+            this.x_1_y.Location = new System.Drawing.Point(177, 68);
+            this.x_1_y.Name = "x_1_y";
+            this.x_1_y.Size = new System.Drawing.Size(100, 22);
+            this.x_1_y.TabIndex = 3;
+            // 
+            // x_1_x
+            // 
+            this.x_1_x.Location = new System.Drawing.Point(71, 68);
+            this.x_1_x.Name = "x_1_x";
+            this.x_1_x.Size = new System.Drawing.Size(100, 22);
+            this.x_1_x.TabIndex = 2;
+            // 
+            // x_2_y
+            // 
+            this.x_2_y.Location = new System.Drawing.Point(177, 96);
+            this.x_2_y.Name = "x_2_y";
+            this.x_2_y.Size = new System.Drawing.Size(100, 22);
+            this.x_2_y.TabIndex = 5;
+            // 
+            // x_2_x
+            // 
+            this.x_2_x.Location = new System.Drawing.Point(71, 96);
+            this.x_2_x.Name = "x_2_x";
+            this.x_2_x.Size = new System.Drawing.Size(100, 22);
+            this.x_2_x.TabIndex = 4;
+            // 
+            // x_3_y
+            // 
+            this.x_3_y.Location = new System.Drawing.Point(177, 124);
+            this.x_3_y.Name = "x_3_y";
+            this.x_3_y.Size = new System.Drawing.Size(100, 22);
+            this.x_3_y.TabIndex = 7;
+            // 
+            // x_3_x
+            // 
+            this.x_3_x.Location = new System.Drawing.Point(71, 124);
+            this.x_3_x.Name = "x_3_x";
+            this.x_3_x.Size = new System.Drawing.Size(100, 22);
+            this.x_3_x.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "x_0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "x_1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "x_2";
+            // 
+            // v
+            // 
+            this.v.AutoSize = true;
+            this.v.Location = new System.Drawing.Point(19, 129);
+            this.v.Name = "v";
+            this.v.Size = new System.Drawing.Size(30, 17);
+            this.v.TabIndex = 13;
+            this.v.Text = "x_3";
+            // 
+            // liczba
+            // 
+            this.liczba.Location = new System.Drawing.Point(126, 152);
+            this.liczba.Name = "liczba";
+            this.liczba.Size = new System.Drawing.Size(100, 22);
+            this.liczba.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "x*";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Lagrange";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Newtone";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 17);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "x";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 17);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "y";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(278, 223);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(157, 223);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(503, 223);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 17);
+            this.label11.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(401, 223);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 17);
+            this.label12.TabIndex = 23;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(623, 223);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(44, 17);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "x - x_i";
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Location = new System.Drawing.Point(17, 257);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(0, 17);
+            this.labelx.TabIndex = 26;
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Location = new System.Drawing.Point(58, 257);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(0, 17);
+            this.labely.TabIndex = 27;
+            // 
+            // first
+            // 
+            this.first.AutoSize = true;
+            this.first.Location = new System.Drawing.Point(157, 257);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(0, 17);
+            this.first.TabIndex = 28;
+            // 
+            // second
+            // 
+            this.second.AutoSize = true;
+            this.second.Location = new System.Drawing.Point(278, 257);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(0, 17);
+            this.second.TabIndex = 29;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(400, 217);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 17);
+            this.label13.TabIndex = 30;
+            // 
+            // third
+            // 
+            this.third.AutoSize = true;
+            this.third.Location = new System.Drawing.Point(401, 257);
+            this.third.Name = "third";
+            this.third.Size = new System.Drawing.Size(0, 17);
+            this.third.TabIndex = 31;
+            // 
+            // fought
+            // 
+            this.fought.AutoSize = true;
+            this.fought.Location = new System.Drawing.Point(503, 257);
+            this.fought.Name = "fought";
+            this.fought.Size = new System.Drawing.Size(0, 17);
+            this.fought.TabIndex = 32;
+            // 
+            // x_i
+            // 
+            this.x_i.AutoSize = true;
+            this.x_i.Location = new System.Drawing.Point(623, 257);
+            this.x_i.Name = "x_i";
+            this.x_i.Size = new System.Drawing.Size(0, 17);
+            this.x_i.TabIndex = 33;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(31, 378);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 17);
+            this.result.TabIndex = 34;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.x_i);
+            this.Controls.Add(this.fought);
+            this.Controls.Add(this.third);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.liczba);
+            this.Controls.Add(this.v);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x_3_y);
+            this.Controls.Add(this.x_3_x);
+            this.Controls.Add(this.x_2_y);
+            this.Controls.Add(this.x_2_x);
+            this.Controls.Add(this.x_1_y);
+            this.Controls.Add(this.x_1_x);
+            this.Controls.Add(this.x_0_y);
+            this.Controls.Add(this.x_0_x);
+            this.Name = "Form1";
+            this.Text = "Interpolacja";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox x_0_x;
+        private System.Windows.Forms.TextBox x_0_y;
+        private System.Windows.Forms.TextBox x_1_y;
+        private System.Windows.Forms.TextBox x_1_x;
+        private System.Windows.Forms.TextBox x_2_y;
+        private System.Windows.Forms.TextBox x_2_x;
+        private System.Windows.Forms.TextBox x_3_y;
+        private System.Windows.Forms.TextBox x_3_x;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label v;
+        private System.Windows.Forms.TextBox liczba;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.Label first;
+        private System.Windows.Forms.Label second;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label third;
+        private System.Windows.Forms.Label fought;
+        private System.Windows.Forms.Label x_i;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interpolacja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelx.Text = "";
+            labely.Text = "";
+            first.Text = "";
+            second.Text = "";
+            third.Text = "";
+            fought.Text = "";
+            x_i.Text = "";
+            result.Text = "";
+
+            label10.Text = "x0-x_i";
+            label9.Text = "x1-x_i";
+            label12.Text = "x2-x_i";
+            label11.Text = "x3-x_i";
+            double[,] tablica = new double[4, 7];
+
+            double x = Double.Parse(liczba.Text);
+
+            tablica[0, 0] = Double.Parse(x_0_x.Text);
+            tablica[1, 0] = Double.Parse(x_1_x.Text);
+            tablica[2, 0] = Double.Parse(x_2_x.Text);
+            tablica[3, 0] = Double.Parse(x_3_x.Text);
+
+            tablica[0, 1] = Double.Parse(x_0_y.Text);
+            tablica[1, 1] = Double.Parse(x_1_y.Text);
+            tablica[2, 1] = Double.Parse(x_2_y.Text);
+            tablica[3, 1] = Double.Parse(x_3_y.Text);
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 2] = tablica[0,0] - tablica[i, 0];
+            }
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 3] = tablica[1, 0] - tablica[i, 0];
+            }
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 4] = tablica[2, 0] - tablica[i, 0];
+            }
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 5] = tablica[3, 0] - tablica[i, 0];
+            }
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 6] = x - tablica[i, 0];
+            }
+
+            double f_x = tablica[0, 1] * (tablica[1, 6] * tablica[2, 6] * tablica[3, 6]) / (tablica[1, 2] * tablica[2, 2] * tablica[3, 2]) +
+                 tablica[1, 1] * (tablica[0, 6] * tablica[2, 6] * tablica[3, 6]) / (tablica[0, 3] * tablica[2, 3] * tablica[3, 3]) +
+                 tablica[2, 1] * (tablica[0, 6] * tablica[1, 6] * tablica[3, 6]) / (tablica[0, 4] * tablica[1, 4] * tablica[3, 4]) +
+                 tablica[3, 1] * (tablica[0, 6] * tablica[2, 6] * tablica[1, 6]) / (tablica[0, 5] * tablica[2, 5] * tablica[1, 5]);
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                labelx.Text += tablica[i, 0]+"\n";
+                labely.Text += tablica[i, 1] + "\n";
+                first.Text += tablica[i, 2] + "\n";
+                second.Text += tablica[i, 3] + "\n";
+                third.Text += tablica[i, 4] + "\n";
+                fought.Text += tablica[i, 5] + "\n";
+                x_i.Text += tablica[i, 6] + "\n";
+
+            }
+            result.Text = $"f(x) = {f_x}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelx.Text = "";
+            labely.Text = "";
+            first.Text = "";
+            second.Text = "";
+            third.Text = "";
+            fought.Text = "";
+            x_i.Text = "";
+            result.Text = "";
+
+            label10.Text = "x_i,x_i+1";
+            label9.Text = "x_i,x_i+1,x_i+2";
+            label12.Text = "x_i,x_i+1,x_i+2,x_i+3";
+            label11.Text = "";
+            double[,] tablica = new double[4, 6];
+
+            double x = Double.Parse(liczba.Text);
+
+            tablica[0, 0] = Double.Parse(x_0_x.Text);
+            tablica[1, 0] = Double.Parse(x_1_x.Text);
+            tablica[2, 0] = Double.Parse(x_2_x.Text);
+            tablica[3, 0] = Double.Parse(x_3_x.Text);
+
+            tablica[0, 1] = Double.Parse(x_0_y.Text);
+            tablica[1, 1] = Double.Parse(x_1_y.Text);
+            tablica[2, 1] = Double.Parse(x_2_y.Text);
+            tablica[3, 1] = Double.Parse(x_3_y.Text);
+
+            for (int i = 0; i < tablica.GetLength(0) - 1; i++)
+            {
+                tablica[i, 2] = (tablica[i + 1, 1] - tablica[i, 1]) / (tablica[i + 1, 0] - tablica[i, 0]);
+            }
+
+            for (int i = 0; i < tablica.GetLength(0) - 2; i++)
+            {
+                tablica[i, 3] = (tablica[i + 1, 2] - tablica[i, 2]) / (tablica[i + 2, 0] - tablica[i, 0]);
+            }
+            tablica[0, 4] = (tablica[1, 3] - tablica[0, 3]) / (tablica[3, 0] - tablica[0, 0]);
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                tablica[i, 5] = x - tablica[i, 0];
+            }
+
+            double f_x = tablica[0, 1] + tablica[0, 2] * tablica[0, 5] + tablica[0, 3] * tablica[0, 5] * tablica[1, 5] + tablica[0, 4] * tablica[0, 5] *
+                tablica[1, 5] * tablica[2, 5];
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                labelx.Text += tablica[i, 0] + "\n";
+                labely.Text += tablica[i, 1] + "\n";
+                x_i.Text += tablica[i, 5] + "\n";
+            }
+            for (int i = 0; i < tablica.GetLength(0) - 1; i++)
+            {
+                first.Text += tablica[i, 2] + "\n";
+            }
+            for (int i = 0; i < tablica.GetLength(0) - 2; i++)
+            {
+                second.Text += tablica[i, 3] + "\n";
+            }
+            for (int i = 0; i < tablica.GetLength(0) - 3; i++)
+            {
+                third.Text += tablica[i, 4] + "\n";
+            }
+            result.Text = $"f(x) = {f_x}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
